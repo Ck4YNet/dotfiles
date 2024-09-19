774a5b28be524b2d3d4c88cec761f94ce5e4a2ff
CK3Y <Diff3454-proton.me>
2024-09-19 19:08:58 +0200

    add walls

--- config/nvim/lua/plugins/code-runner.lua
+++ config/nvim/lua/plugins/code-runner.lua
@@ -4,12 +4,36 @@ return {
     "CRAG666/code_runner.nvim",
     config = function()
       require("code_runner").setup({
-        -- Configuración personalizada aquí
         filetype = {
           python = "python3 -u", -- Comando para ejecutar archivos Python
           javascript = "node", -- Comando para ejecutar archivos JavaScript
           lua = "lua", -- Comando para ejecutar archivos Lua
-          -- Añade más configuraciones según necesites
+          bash = "bash", -- Comando para ejecutar archivos Bash
+          rust = {
+            "cd $dir &&", -- Cambia al directorio del archivo
+            "rustc $fileName &&", -- Compila el archivo Rust
+            "$dir/$fileNameWithoutExt", -- Ejecuta el archivo compilado
+          },
+          c = function(...)
+            local c_base = {
+              "cd $dir &&", -- Cambia al directorio del archivo
+              "gcc $fileName -o", -- Compila el archivo C
+              "/tmp/$fileNameWithoutExt", -- Ubicación temporal para el ejecutable
+            }
+            local c_exec = {
+              "&& /tmp/$fileNameWithoutExt &&", -- Ejecuta el archivo compilado
+              "rm /tmp/$fileNameWithoutExt", -- Elimina el archivo temporal después de la ejecución
+            }
+            vim.ui.input({ prompt = "Add more args:" }, function(input)
+              c_base[4] = input
+              require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+            end)
+          end,
+          java = {
+            "cd $dir &&", -- Cambia al directorio del archivo
+            "javac $fileName &&", -- Compila el archivo Java
+            "java $fileNameWithoutExt", -- Ejecuta el archivo compilado (sin extensión)
+          },
         },
         mode = "term", -- Usar una terminal integrada para la salida
         focus = true, -- Enfocar la terminal después de ejecutar
